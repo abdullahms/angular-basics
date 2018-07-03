9b0a11c966a85f0e070874edd07851f205d5bf28
Abdullah Gulam Hind <abdullah.gulam@zensar.com>
2018-07-03 16:57:04 +0530

    Linking Routes

--- BaseProject/src/app/shared/workshop.service.ts
+++ BaseProject/src/app/shared/workshop.service.ts
@@ -99,7 +99,7 @@ const WORKSHOPS = [
     date: '4/15/2037',
     time: '9:00 am',
     price: 950.00,
-    imageUrl: '/app/assets/images/ng-nl.png',
+    imageUrl: '../assets/images/ng-nl.png',
     // location: {
     //   address: 'The NG-NL Convention Center & Scuba Shop',
     //   city: 'Amsterdam',
@@ -160,7 +160,7 @@ const WORKSHOPS = [
     date: '5/4/2037',
     time: '9:00 am',
     price: 759.00,
-    imageUrl: '/app/assets/images/ng-conf.png',
+    imageUrl: '../assets/images/ng-conf.png',
     location: {
       address: 'The Palatial America Hotel',
       city: 'Salt Lake City',
@@ -242,7 +242,7 @@ const WORKSHOPS = [
     date: '6/10/2037',
     time: '8:00 am',
     price: 800.00,
-    imageUrl: '/app/assets/images/basic-shield.png',
+    imageUrl: '../assets/images/basic-shield.png',
     location: {
       address: 'The UN Angular Center',
       city: 'New York',
@@ -291,7 +291,7 @@ const WORKSHOPS = [
     date: '2/10/2037',
     time: '9:00 am',
     price: 400.00,
-    imageUrl: '/app/assets/images/ng-vegas.png',
+    imageUrl: '../assets/images/ng-vegas.png',
     location: {
       address: 'The Excalibur',
       city: 'Las Vegas',
